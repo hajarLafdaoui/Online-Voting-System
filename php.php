ff91241ad9d98d4e97df5f713ca2885732df9f20
hajarLafdaoui <155734398+hajarLafdaoui@users.noreply.github.com>
2024-08-18 23:48:12 +0100

    connection to my database

--- php.php
+++ php.php
@@ -1,1 +1,13 @@
+<?php
+$dsn = 'mysql:host=localhost;dbname=voting;charset=utf8';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 
